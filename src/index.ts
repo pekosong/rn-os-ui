e961bb9b2279d81fa573853d57dcbdb05623ef55
song <peko@ip-172-31-43-162.ap-northeast-2.compute.internal>
2021-04-23 12:25:27 +0900

    export adeed

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { default as View } from "./components/View";
+export { default as Image } from "./components/Image";
+export { default as ScrollView } from "./components/ScrollView";
+export { default as Text } from "./components/Text";
+export { default as TouchableOpacity } from "./components/TouchableOpacity";
